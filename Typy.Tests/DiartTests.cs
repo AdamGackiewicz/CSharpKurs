67e4d6aef1c18e05dcae11b01de692aa6730a33e
Adam Gackiewicz <adam.gackiewicz@gmail.com>
2020-03-31 22:23:29 +0200

    testy rozbudowa

---
+++ Typy.Tests/DiartTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Typy.Tests
+{
+    [TestClass]
+    public class DiaryTest
+    {
+        [TestMethod]
+        public void CalculateMaxGrade()
+        {
+            Diary diary = new Diary();
+            diary.AddRating(9f);
+            diary.AddRating(8f);
+            diary.AddRating(7f);
+            diary.AddRating(5f);
+            diary.AddRating(3f);
+
+            DiaryStatistics stats = diary.ComputeStatistics();
+
+            Assert.AreEqual(9, stats.MaxGrade);
+
+            //bool value = false;
+            //Assert.IsTrue(value);
+        }
+    }
+}
